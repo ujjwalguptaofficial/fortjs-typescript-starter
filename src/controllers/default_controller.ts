4c95e7e75cf622e53f6f17aa5867106e196d13cf
Ujjwal Gupta <ujjwalkumargupta4u@gmail.com>
2018-12-20 11:04:34 +0530

    added the suggestion for saving exception

--- src/controllers/default_controller.ts
+++ src/controllers/default_controller.ts
@@ -9,6 +9,7 @@ export class DefaultController extends Controller {
         } catch (ex) {
             console.log(ex);
             // handle exception and show user a good message.
+            // save this ex in a file or db, so that you can know what's the issue and where
             const result = await textResult(`Our server is busy right now. Please try later.`);
             return result;
         }
