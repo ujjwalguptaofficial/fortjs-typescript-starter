7af6d145ecc2a0b297eadf67320b08f9f3e4c72b
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2019-06-13 09:54:16 +0530

    installed latest version of fortjs

--- controllers/default_controller.ts
+++ controllers/default_controller.ts
@@ -5,13 +5,12 @@ export class DefaultController extends Controller {
     @DefaultWorker()
     async index() {
         try {
-            const model = {
+            const data = {
                 title: 'FortJs'
             };
-            const result = await viewResult('default/index.html', model);
+            const result = await viewResult('default/index.html', data);
             return result;
         } catch (ex) {
-            console.log(ex);
             // handle exception and show user a good message.
             // save this ex in a file or db, so that you can know what's the issue and where
             const result = await textResult("Our server is busy right now. Please try later.");
