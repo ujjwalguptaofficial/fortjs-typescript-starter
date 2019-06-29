13489dceb29820ab79619dc79bf5d339e54f976b
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2019-06-29 19:45:44 +0530

    updated fortjs version & related changes

--- routes.ts
+++ routes.ts
@@ -2,6 +2,6 @@ import { DefaultController } from "./controllers/default_controller";
 import { ParentRoute } from "fortjs";
 
 export const routes: ParentRoute[] = [{
-    path: "/default",
+    path: "*",
     controller: DefaultController
 }];
