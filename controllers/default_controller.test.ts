b87b263205b4c22a2a8705be40f8f87af37d5e8f
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2019-10-05 22:30:05 +0530

    installed fortjs@1.8.0

--- controllers/default_controller.test.ts
+++ controllers/default_controller.test.ts
@@ -4,16 +4,18 @@ import { createApp } from "../index";
 
 describe('DefaultController', () => {
     let app: Fort;
+    let controller: DefaultController;
     beforeAll(async () => {
-        app = await createApp() as any;
+        app = await createApp();
+        controller = new DefaultController();
     });
 
     it('index', async () => {
         const expectedResult = await viewResult('default/index.html', {
             title: 'FortJs'
         });
-        const indexMethodOutput = await new DefaultController().index('FortJs');
-        expect(indexMethodOutput).toEqual(expectedResult);
+        const result = await controller.index('FortJs');
+        expect(result).toEqual(expectedResult);
     });
 
     afterAll(() => {
